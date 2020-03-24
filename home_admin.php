c50cbb81616141a4e04e288f0b7d1e933239f089
kedaranugerahnachaputra <54616016+kedaranugerahnachaputra@users.noreply.github.com>
2020-03-24 19:52:28 +0700

    Add files via upload

---
+++ home_admin.php
@@ -0,0 +1,47 @@
+<?php
+    session_start();
+
+    if(!isset($_SESSION['username'])){
+        header("Location: index.php?acces_denied");
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>User Page</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=PT+Sans&display=swap" rel="stylesheet">
+    <style>
+        .welcome{
+            color: #fff;
+            font-size: 35pt;
+            font-weight: 900;
+        }
+    </style>
+</head>
+<body>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <a href="home_admin.php" class="navbar-brand">Beranda</a>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+            <div class="navbar-nav">
+                <a href="home_admin.php" class="nav-item nav-link active">Home</a>
+                <a href="#" class="nav-item nav-link">Buku</a>
+                <a href="#" class="nav-item nav-link">User</a>
+                <a href="#" class="nav-item nav-link">Admin</a>
+            </div>
+            <form class="form-inline ml-auto">
+                <div class="navbar-nav">
+                    <a href="#" class="nav-item nav-link active">Logout</a>
+                </div>
+            </form>
+        </div>
+    </nav>
+    <header>
+        <h1 class="welcome">Selamat Datang <?php echo $_SESSION['username'];?></h1>
+    </header>
+</body>
+</html>
